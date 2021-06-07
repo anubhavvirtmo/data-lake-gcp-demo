80cef8db7c32fb40b99d4b21c2543231bf4bb106
Anubhav Choudhary <46858153+anubhavvirtmo@users.noreply.github.com>
2021-06-07 21:07:37 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -30,3 +30,34 @@ resource "google_storage_bucket" "gcs-data-lake-backup" {
   force_destroy = true
   storage_class = "STANDARD"
 }
+resource "google_storage_bucket_acl" "gcs-data-lake-landing-acl" {
+  bucket = google_storage_bucket.gcs-data-lake-landing.name
+  role_entity = [
+    "OWNER:${local.unique_id}-de@gmail.com",
+    "READER:${local.unique_id}-ds@gmail.com",
+  ]
+}
+
+resource "google_storage_bucket_acl" "gcs-data-lake-sensitive-acl" {
+  bucket = google_storage_bucket.gcs-data-lake-sensitive.name
+  role_entity = [
+    "OWNER:${local.unique_id}-de@gmail.com",
+    "READER:${local.unique_id}-ds@gmail.com",
+  ]
+}
+
+resource "google_storage_bucket_acl" "gcs-data-lake-work-acl" {
+  bucket = google_storage_bucket.gcs-data-lake-work.name
+  role_entity = [
+    "OWNER:${local.unique_id}-de@gmail.com",
+    "WRITER:${local.unique_id}-ds@gmail.com",
+  ]
+}
+
+resource "google_storage_bucket_acl" "gcs-data-lake-backup-acl" {
+  bucket = google_storage_bucket.gcs-data-lake-backup.name
+  role_entity = [
+    "OWNER:${local.unique_id}-de@gmail.com",
+    "READER:${local.unique_id}-ds@gmail.com",
+  ]
+}
